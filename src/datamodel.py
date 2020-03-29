e7ea7039a5dbabd486cedfe63c93ea5a378bf561
maxnth <maximilian.noeth@protonmail.com>
2020-03-30 00:07:28 +0200

    add Docstrings

--- src/datamodel.py
+++ src/datamodel.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union
+from typing import Dict, List, Type, Union
 from pathlib import Path
 
 from lxml import etree
@@ -7,41 +7,16 @@ import numpy as np
 ns_dict = {"tei": "http://www.tei-c.org/ns/1.0"}
 
 
-class Drama:
-    def __init__(self, drama: Union[str, Path]):
-        self.tree = self.get_root_tree(drama)
-
-        self.character_map = self.get_character_mapping()
-        self.base_adjacency_matrix = self.build_base_adjacency_matrix()
-        self.scenes = self.get_scenes()
-        self.aggregate_adjacency_matrix = self.build_aggregate_adjacency_matrix()
-
-    def get_root_tree(self, path: Union[str, Path]):
-        return etree.parse(path).getroot() if isinstance(path, str) else etree.parse(str(path)).getroot()
-
-    def get_character_mapping(self):
-        characters = {sp.replace("#", "") for sp in self.tree.xpath("//tei:sp/@who", namespaces=ns_dict)}
-        return {character: num for (num, character) in enumerate(characters)}
-
-    def build_base_adjacency_matrix(self):
-        return np.zeros((len(self.character_map), len(self.character_map)))
-
-    def get_scenes(self):
-        scenes = self.tree.xpath('.//tei:div[@type="scene"]', namespaces=ns_dict)
-
-        if len(scenes) == 0:
-            scenes = self.tree.xpath('.//tei:div[@type="act"]', namespaces=ns_dict)
-
-        assert len(scenes) > 0, "Drama contains neither acts nor scenes."
-
-        return [Scene(elem, self.character_map, self.base_adjacency_matrix) for elem in scenes]
-
-    def build_aggregate_adjacency_matrix(self):
-        scene_matrices = [scene.adjacency_matrix for scene in self.scenes]
-        return sum(scene_matrices)
-
-
 class Scene:
+    """Representation of a scene in a drama for temporal network analysis.
+
+    Args:
+        drama_scene (etree.Element): lxml etree element containing all TEI encoded XML of the corresponding scene.
+        character_map (Dict[str, int]): Dictionary with names of characters which appear as speakrs in the drama as key
+                                        and a unique integer as value. The latter is used for mapping the indices of
+                                        the calculated matrices to the characters.
+        base_adjacency_matrix (np.ndarray): Base adjacency matrix of all characters for the drama (filled with zeroes).
+    """
     def __init__(self, drama_scene: etree.Element, character_map: Dict[str, int],
                  base_adjacency_matrix: np.ndarray):
         self.tree = drama_scene
@@ -51,6 +26,13 @@ class Scene:
         self.fill_adjacency_matrix()
 
     def fill_adjacency_matrix(self):
+        """Fills adjacency matrix based on the communication between speaking characters in a scene.
+
+        Note:
+            Communication between characters is assumed when the speaking acts of two characters are in direct
+            neighborhood alas 
+
+        """
         speech_acts = [speech_act.replace("#", "") for speech_act in
                        self.tree.xpath("./tei:sp/@who", namespaces=ns_dict)]
 
@@ -62,3 +44,54 @@ class Scene:
                 second_node = self.character_map[edge[1]]
                 self.adjacency_matrix[first_node][second_node] += 1
                 self.adjacency_matrix[second_node][first_node] += 1
+
+
+class Drama:
+    """Representation of a drama for temporal network analysis.
+
+    Args:
+        drama (Union[str, Path]): Path to a file containing a TEI encoded drama
+
+    Attribute:
+        tree (etree.Element): lxml etree element containing the root tree representation of the TEI encoded XML
+        character_map (dict): Dictionary with names of characters which appear as speakrs in the drama as key and a
+                            unique integer as value. The latter is used for mapping the indices of the calculated
+                            matrices to the characters.
+        base_adjacency_matrix (np.ndarray): Base adjacency matrix of all characters for the drama (filled with zeroes).
+        scenes (List[Scene]): List of all scenes in the drama, represented by instances of the Scene class.
+                                    If a drama doesn't contain any scenes, acts are used as substitute.
+        aggregate_adjacency_matrix (np.ndarray): An adjacancy matrix representing the aggregated frequency of
+                                                communication between characters.
+    """
+    def __init__(self, drama: Union[str, Path]):
+        self.tree = self.get_root_tree(drama)
+
+        self.character_map = self.get_character_mapping()
+        self.base_adjacency_matrix = self.build_base_adjacency_matrix()
+        self.scenes = self.get_scenes()
+        self.aggregate_adjacency_matrix = self.build_aggregate_adjacency_matrix()
+
+    def get_root_tree(self, path: Union[str, Path]) -> etree.Element:
+        return etree.parse(path).getroot() if isinstance(path, str) else etree.parse(str(path)).getroot()
+
+    def get_character_mapping(self) -> Dict[str, int]:
+        characters = {sp.replace("#", "") for sp in self.tree.xpath("//tei:sp/@who", namespaces=ns_dict)}
+        return {character: num for (num, character) in enumerate(characters)}
+
+    def build_base_adjacency_matrix(self) -> np.ndarray:
+        return np.zeros((len(self.character_map), len(self.character_map)))
+
+    def get_scenes(self) -> List[Scene]:
+        scenes = self.tree.xpath('.//tei:div[@type="scene"]', namespaces=ns_dict)
+
+        if len(scenes) == 0:
+            scenes = self.tree.xpath('.//tei:div[@type="act"]', namespaces=ns_dict)
+
+        assert len(scenes) > 0, "Drama contains neither acts nor scenes."
+
+        return [Scene(elem, self.character_map, self.base_adjacency_matrix) for elem in scenes]
+
+    def build_aggregate_adjacency_matrix(self) -> np.ndarray:
+        scene_matrices = [scene.adjacency_matrix for scene in self.scenes]
+        return sum(scene_matrices)
+
