e633494c371d8615068072cda4b2b8878ba9282d
maxnth <maximilian.noeth@protonmail.com>
2020-03-31 13:18:56 +0200

    add class for aggregate graph

--- src/temporalNetwork.py
+++ src/temporalNetwork.py
@@ -1,4 +1,38 @@
-from scipy import linalg
+import statistics
+
+import numpy as np
+import pandas as pd
+
+import networkx as nx
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+class AggregateGraph:
+    def __init__(self, drama):
+        self.drama = drama
+        self.base_centrality = self.get_eigenvector_centrality(self.drama.export_graph())
+
+    @staticmethod
+    def get_eigenvector_centrality(graph, mean=True) -> dict:
+        if mean:
+            return statistics.mean(nx.eigenvector_centrality(graph).values())
+        return nx.eigenvector_centrality(graph)
+
+    def get_vitalities(self) -> dict:
+        vitalities = dict()
+
+        for character in self.drama.character_map.keys():
+            _graph = self.drama.export_graph().copy()
+            _graph.remove_node(character)
+
+            vitalities[character] = self.base_centrality - self.get_eigenvector_centrality(_graph)
+
+        return vitalities
+
+    def plot_vitalities(self):
+        pass
 
 
 class TemporalGraph:
